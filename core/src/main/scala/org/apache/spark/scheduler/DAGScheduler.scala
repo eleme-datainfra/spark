955554a0ab772486716540c00aa94756c1dd83f2
cenyuhai <cenyuhai@diditaxi.com.cn>
2015-07-22 11:26:25 +0800

    测试的时候发现，只修改paritiion个数为200的rdd会存在问题，所以改成最初始的状态

--- core/src/main/scala/org/apache/spark/scheduler/DAGScheduler.scala
+++ core/src/main/scala/org/apache/spark/scheduler/DAGScheduler.scala
@@ -914,8 +914,7 @@ class DAGScheduler(
     if(isAutoPartition){
       if (stageIdToChildStage.contains(stage.id)) {
         val childStage = stageIdToChildStage.get(stage.id)
-        if (!childStage.get.isResetNumPartitions
-          && childStage.get.numPartitions == sc.getConf.getInt("spark.sql.shuffle.partitions", 200)) {
+        if (!childStage.get.isResetNumPartitions && childStage.get.numPartitions > 1) {
           updateNumPartitionsOfStage(childStage.get, estimateNumPartitionsOfStage(childStage.get))
         } else {
           logInfo(stage + "'s child " + childStage + " have been estimated, partitionNumber=" +
