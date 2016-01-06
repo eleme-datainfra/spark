bb74902a7c04b6c64c4829dc0416f677f341638b
cenyuhai <cenyuhai@didichuxing.com>
2016-01-06 21:52:54 +0800

    忘记转换类型了，把类型转换一下

--- core/src/main/scala/org/apache/spark/storage/BlockManagerSlaveEndpoint.scala
+++ core/src/main/scala/org/apache/spark/storage/BlockManagerSlaveEndpoint.scala
@@ -22,7 +22,7 @@ import scala.concurrent.{ExecutionContext, Future}
 import org.apache.spark.{Logging, MapOutputTracker, SparkEnv}
 import org.apache.spark.rpc.{RpcCallContext, RpcEnv, ThreadSafeRpcEndpoint}
 import org.apache.spark.storage.BlockManagerMessages._
-import org.apache.spark.util.{ThreadUtils, Utils}
+import org.apache.spark.util.{Metrics, ThreadUtils, Utils}
 
 /**
  * An RpcEndpoint to take commands from the master to execute options. For example,
@@ -75,7 +75,7 @@ class BlockManagerSlaveEndpoint(
       context.reply(Utils.getThreadDump())
 
     case GetMetrics =>
-      context.reply(Utils.getMetrics())
+      context.reply(Metrics(Utils.getMetrics()))
   }
 
   private def doAsync[T](actionMessage: String, context: RpcCallContext)(body: => T) {
