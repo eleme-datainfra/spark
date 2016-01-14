2ec34dac81d2419949dc9d0c006bd194d145da32
cenyuhai <cenyuhai@didichuxing.com>
2016-01-14 22:34:37 +0800

    从restoreFile恢复的时候，把offset等相关信息输出

--- external/kafka/src/main/scala/org/apache/spark/streaming/kafka/KafkaUtils.scala
+++ external/kafka/src/main/scala/org/apache/spark/streaming/kafka/KafkaUtils.scala
@@ -533,8 +533,8 @@ object KafkaUtils extends Logging {
             }
           }
           if(fromOffsets.size == offsets.size) {
-            logInfo("Reading offset from file:" + file.getPath.toString)
-            offsets.foreach(o => logInfo(s"Partition  ${o._1.partition} -> offset ${o._2}"))
+            println("Reading offset from file:" + file.getPath.toString)
+            offsets.foreach(o => println(s"Partition  ${o._1.partition} -> offset ${o._2}"))
             return offsets
           }
           logWarning("Skip file " + file.getPath.toString + ", partition size is not equal!")
