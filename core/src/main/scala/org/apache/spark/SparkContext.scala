7dc99f9e7a09de1d174641d80eb4376f3bb6b241
cenyuhai <cenyuhai@didichuxing.com>
2016-07-02 21:07:37 +0800

    修改无法打印ApplicationId的问题

--- core/src/main/scala/org/apache/spark/SparkContext.scala
+++ core/src/main/scala/org/apache/spark/SparkContext.scala
@@ -553,7 +553,7 @@ class SparkContext(config: SparkConf) extends Logging with ExecutorAllocationCli
     _ui.foreach(_.setAppId(_applicationId))
     _env.blockManager.initialize(_applicationId)
     // scalastyle:off println
-    println(s"ApplicationId is $_applicationId")
+    println(s"ApplicationId is ${_applicationId}")
     // scalastyle:on println
 
     // The metrics system for Driver need to be set spark.app.id to app ID.
