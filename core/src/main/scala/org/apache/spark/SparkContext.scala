b1b9aae8b37fe23089ec55ddde5bbc80328d4bdd
cenyuhai <cenyuhai@didichuxing.com>
2016-07-17 13:59:44 +0800

    修改输出日志的格式

--- core/src/main/scala/org/apache/spark/SparkContext.scala
+++ core/src/main/scala/org/apache/spark/SparkContext.scala
@@ -554,7 +554,7 @@ class SparkContext(config: SparkConf) extends Logging with ExecutorAllocationCli
     _ui.get.appUIAddress
     _env.blockManager.initialize(_applicationId)
     // scalastyle:off println
-    System.err.println(s"Application ID is ${_applicationId}, trackingURL:${_ui.get.appUIAddress}")
+    System.err.println(s"Application ID is ${_applicationId}, trackingURL: ${_ui.get.appUIAddress}")
     // scalastyle:on println
 
     // The metrics system for Driver need to be set spark.app.id to app ID.
