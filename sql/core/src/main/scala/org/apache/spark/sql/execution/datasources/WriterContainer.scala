ca6ce41f50f5b0bf78d0ec4b63bcb497165f50ae
cenyuhai <cenyuhai@didichuxing.com>
2016-07-17 13:52:50 +0800

    避免生成空文件

--- sql/core/src/main/scala/org/apache/spark/sql/execution/datasources/WriterContainer.scala
+++ sql/core/src/main/scala/org/apache/spark/sql/execution/datasources/WriterContainer.scala
@@ -249,6 +249,7 @@ private[sql] class DefaultWriterContainer(
   extends BaseWriterContainer(relation, job, isAppend) {
 
   def writeRows(taskContext: TaskContext, iterator: Iterator[InternalRow]): Unit = {
+    if (!iterator.hasNext) return
     executorSideSetup(taskContext)
     val configuration = SparkHadoopUtil.get.getConfigurationFromJobContext(taskAttemptContext)
     configuration.set("spark.sql.sources.output.path", outputPath)
