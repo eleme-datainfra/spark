847371fa7620e3df2c1a7a773442dfc5cab6ec8b
cenyuhai <cenyuhai@didichuxing.com>
2016-07-29 03:01:35 +0800

    保护numRunningTasks是一个大于等于0的数字

--- core/src/main/scala/org/apache/spark/ExecutorAllocationManager.scala
+++ core/src/main/scala/org/apache/spark/ExecutorAllocationManager.scala
@@ -601,7 +601,7 @@ private[spark] class ExecutorAllocationManager(
         if (stageIdToNumTasks.isEmpty) {
           allocationManager.onSchedulerQueueEmpty()
           if (numRunningTasks != 0) {
-            logWarning("No stages are running, but numRunningTasks != 0")
+            logWarning(s"No stages are running, but numRunningTasks = ${numRunningTasks}")
             numRunningTasks = 0
           }
           executorIdToTaskIds.clear()
@@ -646,7 +646,11 @@ private[spark] class ExecutorAllocationManager(
       val taskIndex = taskEnd.taskInfo.index
       val stageId = taskEnd.stageId
       allocationManager.synchronized {
-        numRunningTasks -= 1
+        if (numRunningTasks > 0) {
+          numRunningTasks -= 1
+        } else {
+          logInfo("SparkListenerTaskEnd got, but numRunningTasks == 0")
+        }
 
         // If the executor is no longer running any scheduled tasks, mark it as idle
         if (executorIdToTaskIds.contains(executorId)) {
