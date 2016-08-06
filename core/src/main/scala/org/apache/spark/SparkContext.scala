c170eae1d348ddf2367904d3325360132b4648bb
cenyuhai <cenyuhai@didichuxing.com>
2016-08-06 23:49:31 +0800

    修复sparkcontext的SparkUI为None导致JAVA8 test的错误

--- core/src/main/scala/org/apache/spark/SparkContext.scala
+++ core/src/main/scala/org/apache/spark/SparkContext.scala
@@ -553,7 +553,7 @@ class SparkContext(val config: SparkConf) extends Logging with ExecutorAllocatio
     _ui.foreach(_.setAppId(_applicationId))
     _env.blockManager.initialize(_applicationId)
     // scalastyle:off println
-    System.err.println(s"Application ID is ${_applicationId}, trackingURL: ${_ui.get.appUIAddress}")
+    System.err.println(s"Application ID is ${_applicationId}, trackingURL: ${_ui.map(_.appUIAddress)}")
     // scalastyle:on println
 
     // The metrics system for Driver need to be set spark.app.id to app ID.
