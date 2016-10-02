f3b5c7b77a988fbf9930e2d3bef818a17aacdd73
cenyuhai <cenyuhai@didichuxing.com>
2016-10-02 13:17:46 +0800

    修复不区分表格式都用orc格式来读的问题

--- sql/hive/src/main/scala/org/apache/spark/sql/hive/TableReader.scala
+++ sql/hive/src/main/scala/org/apache/spark/sql/hive/TableReader.scala
@@ -23,7 +23,7 @@ import org.apache.hadoop.fs.{Path, PathFilter}
 import org.apache.hadoop.hive.conf.HiveConf
 import org.apache.hadoop.hive.metastore.api.hive_metastoreConstants._
 import org.apache.hadoop.hive.ql.exec.Utilities
-import org.apache.hadoop.hive.ql.io.orc.OrcNewInputFormat
+import org.apache.hadoop.hive.ql.io.orc.{OrcInputFormat, OrcNewInputFormat}
 import org.apache.hadoop.hive.ql.metadata.{Partition => HivePartition, Table => HiveTable, HiveUtils}
 import org.apache.hadoop.hive.ql.plan.TableDesc
 import org.apache.hadoop.hive.serde2.Deserializer
@@ -230,7 +230,9 @@ class HadoopTableReader(
         }
       }
 
-      if (sc.conf.useFasterOrcReader) {
+      if (sc.conf.useFasterOrcReader
+          && partDesc.getInputFileFormatClass.isInstanceOf[OrcInputFormat]) {
+
         val inputFormatClass = classOf[OrcNewInputFormat]
           .asInstanceOf[Class[_ <: NewInputFormat[NullWritable, InternalRow]]]
         val initializeJobConfFunc =
