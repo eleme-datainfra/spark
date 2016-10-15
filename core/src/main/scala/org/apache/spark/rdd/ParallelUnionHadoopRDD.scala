a60dcc9667a6feb764257360c8cdd8577573df1e
cenyuhai <261810726@qq.com>
2016-10-15 12:42:49 +0800

    把结果包装成一个ByteBuffer

--- core/src/main/scala/org/apache/spark/rdd/ParallelUnionHadoopRDD.scala
+++ core/src/main/scala/org/apache/spark/rdd/ParallelUnionHadoopRDD.scala
@@ -56,8 +56,6 @@ class ParallelUnionHadoopRDD[T: ClassTag](
       val initJobConfFuncOpt = initLocalJobConfFuncOpt
       val partitionsWithIndex = partitionInfos.zipWithIndex.toArray
 
-      val sparkConf = sc.conf
-
       val rddIndexWithPartitions =
         sc.parallelize(partitionsWithIndex, partitionInfos.size).map { case (part, index) =>
           val jobConfCacheKey = "rdd_%d_job_conf".format(rddIdMap(index))
@@ -79,6 +77,7 @@ class ParallelUnionHadoopRDD[T: ClassTag](
           for (i <- 0 until inputSplits.size) {
             array(i) = new HadoopPartition(rddIdMap(index), i, inputSplits(i))
           }
+          val sparkConf = SparkEnv.get.conf
           val javaSerializer = new JavaSerializer(sparkConf).newInstance()
           val byteBuffer = javaSerializer.serialize((index, array))
           byteBuffer
@@ -86,6 +85,7 @@ class ParallelUnionHadoopRDD[T: ClassTag](
 
       val array = new ArrayBuffer[UnionPartition[T]]()
       var pos = 0
+      val sparkConf = SparkEnv.get.conf
       val javaSerializer = new JavaSerializer(sparkConf).newInstance()
       rddIndexWithPartitions.foreach { b =>
         val (rddIndex, splits) = javaSerializer.deserialize[(Int, Array[Partition])](b)
